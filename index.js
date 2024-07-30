ab84031aebc5f5df8b02477ff07cc7e8ea7ddf72
slomec <slomeec@gmail.com>
2024-07-30 20:33:59 +0500

    new design

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 //Parameters
 let blockSize = 100;
 let cutBotomTails = false;
-let colorsPalette = [
-  [255, 32, 78],
-  [160, 21, 62],
-  [93, 14, 65],
-  [0, 34, 77],
-];
+let mainColor = 240;
+let range = 50;
 
 let widthBlockSize;
 let heightBlockSize;
 let width;
 let height;
+
+let colorsPalette = [];
+for (let i = mainColor - range / 2; i <= mainColor + range / 2; i++) {
+  colorsPalette.push(i);
+}
+colorsPalette = colorsPalette.concat(colorsPalette.toReversed().splice(1));
 
 function setSizes() {
   if (cutBotomTails) {
@@ -27,34 +29,43 @@ function setSizes() {
   }
 }
 
-function drawTails() {
+function addTiles() {
   document.getElementById("block").remove();
   const block = document.createElement("div");
   block.id = "block";
+  const tiles = [];
   for (let i = 0; i < height * width; i++) {
-    let [c1, c2, c3] = colorsPalette[Math.floor(Math.random() * colorsPalette.length)];
+    tiles.push(Math.floor(Math.random() * range));
     const tile = document.createElement("div");
     tile.id = i;
     //colors
-    tile.style.borderLeft = `10px solid rgb(${c1 + 20}, ${c2 + 20}, ${c3 + 20})`;
-    tile.style.borderTop = `10px solid rgb(${c1 + 30}, ${c2 + 30}, ${c3 + 30})`;
-    tile.style.borderRight = `10px solid rgb(${c1 - 20}, ${c2 - 20}, ${c3 - 20})`;
-    tile.style.borderBottom = `10px solid rgb(${c1 - 30}, ${c2 - 30}, ${c3 - 30})`;
+    tile.style.border = `5px solid black`;
 
     tile.style.boxSizing = `border-box`;
-    tile.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+
     //sizes
     tile.style.width = `${widthBlockSize}px`;
     tile.style.height = `${heightBlockSize}px`;
     block.appendChild(tile);
   }
   document.body.appendChild(block);
+  return tiles;
+}
+
+function drawTiles() {
+  tiles.forEach((color, index) => {
+    document.getElementById(index).style.backgroundColor = `hsl(${colorsPalette[color]}, 70%, 50%`;
+    tiles[index] = (color + Math.floor(Math.random() * 2)) % (colorsPalette.length + 1);
+  });
 }
 
 setSizes();
-drawTails();
+let tiles = addTiles();
 
+setInterval(() => {
+  drawTiles();
+}, 40);
 window.addEventListener("resize", () => {
   setSizes();
-  drawTails();
+  tiles = addTiles();
 });
